afe2eff9568301fd1e821e682f64b98bdca670bc
ConnorFieldUser <conquest.field@gmail.com>
2016-11-09 17:38:46 -0500

    intitailzed django projects

--- urlshortener_project/bookmark_app/views.py
+++ urlshortener_project/bookmark_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
 
 # Create your views here.
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
+
+
+class IndexView(TemplateView):
+        template_name = 'index.html'
